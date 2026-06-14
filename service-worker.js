4375ac47cf04c126867952dd6ff6d9338a64d598
giulioporcu <giulioporcu@users.noreply.github.com>
2026-06-14 05:39:55 +0000

    deploy: 01d81678e434cd904cb503f7c5e8fb4f12dcbdad

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+﻿// In development, always fetch from the network and do not enable offline support.
+// This is because caching would make development more difficult (changes would not
+// be reflected on the next load without a service worker update and force refresh).
+
+self.addEventListener('fetch', () => { });
